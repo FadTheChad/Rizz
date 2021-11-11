49f9d58aec4c85418a030e584024edf5b5fafcf8
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-11 18:11:38 +0500

    command loader yay

---
+++ src/structure/handlers/command.ts
@@ -0,0 +1,24 @@
+import { readdirSync } from 'fs'
+import ZRKClient from '../Client'
+import ICommand from '../interfaces/ICommand'
+
+const handler = (client: ZRKClient) => {
+    for (const folder of client.categories) {
+        if (folder === 'context') continue
+
+        const commandFiles = readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('js') || file.endsWith('ts')) // <= what could this possibly mean!?!?!?!
+        console.log(`\nSearching ${folder} commands...\n`)
+
+        for (const file of commandFiles) {
+            const req = require(`../../commands/${folder}/${file}`)
+
+            const command: ICommand = req.default
+
+            client.commands.set(command.name, command)
+
+            console.log(`\t${file} has been loaded!`)
+        }
+    }
+}
+
+export default handler
