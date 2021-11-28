9ec772916fa5f2ce9e6e50251e69989e21723c77
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-11-28 15:49:45 +0500

    test cmd for embed

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -1,11 +1,28 @@
 import ICommand from '../../structure/interfaces/ICommand'
+import RZEmbed from '../../utils/RZEmbed'
 
 const command: ICommand = {
     name: 'ping',
     description: 'sends ping',
     aliases: 'test',
     async run (client, message, args) {
-        message.channel!.sendMessage(`Pong!\nAPI Latency: ${client.websocket.ping} ms`)
+        let embed = new RZEmbed()
+
+        embed.setTitle('Title')
+        embed.setDescription('this is the description and shit')
+        embed.addFields([{
+            name: 'Field 1',
+            value: 'value 1'
+        }, {
+            name: 'Field 2',
+            value: 'value 2'
+        }, {
+            name: 'Field 3',
+            value: 'value 3'
+        }])
+
+        console.log(embed)
+        message.channel?.sendMessage(embed.toString())
     }
 }
 
