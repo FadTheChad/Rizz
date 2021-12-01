db1606fbb4fb25d45c3b3aece320d9cc5528b371
Fad F <90547391+FadTheChad@users.noreply.github.com>
2021-12-01 23:11:27 +0500

    Updated ping embed

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -7,19 +7,12 @@ const command: ICommand = {
     aliases: 'test',
     async run (client, message, args) {
         let embed = new RZEmbed()
-
-        embed.setTitle('Title')
-        embed.setDescription('this is the description and shit')
-        embed.addFields([{
-            name: 'Field 1',
-            value: 'value 1'
-        }, {
-            name: 'Field 2',
-            value: 'value 2'
-        }, {
-            name: 'Field 3',
-            value: 'value 3'
-        }])
+            .setTitle('Pong!')
+            .addField({
+                name: 'API Latency',
+                value: client.websocket.ping?.toString() ?? 'UNKNOWN'
+            })
+            .setColour('#FFFF00')
 
         console.log(embed)
         message.channel?.sendMessage(embed.toString())
