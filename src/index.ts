8f151583d30f9f90b7842493f0ae4955bd1bf776
Fad F <fmzfads@gmail.com>
2022-01-25 21:58:40 +0500

    removed useless import

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import IConfig from './structure/interfaces/IConfig'
 const config: IConfig = _config
 
 import RZClient from './structure/Client'
-import { Message } from 'revolt.js/dist/maps/Messages'
 
 const client = new RZClient(config)
 
